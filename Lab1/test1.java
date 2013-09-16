300bc7cd5df583eb6496d41674be3238adad9c9d
trangdth55 <trangdth_55@vnu.edu.vn>
2013-09-16 16:27:11 +0700

    upload them
    jnầlfkà

--- Lab1/test1.java
+++ Lab1/test1.java
@@ -1,1 +1,27 @@
-﻿
+﻿public class test1
+{
+	int giaiPTB1(int a, int b){
+	}
+	static void test3() {
+		int ketquaThat = giaiPTB1(1,1);
+		if(ketQuaThat == -1){
+			System.out.println("test3 was success");
+		}
+		else{
+			System.out.println("test3 was failed");
+		}
+	}
+	static void test4(){
+		int ketquaThat = gaiPTB1(10, -90);
+		if(ketquaThat == 9){
+		}
+			System.out.println("test4 was success");
+		else {
+			System.out.println("failed");
+		}
+	}
+	public static void main(String [] args)
+	{
+		System.out.println("Hello World");
+	}
+}
